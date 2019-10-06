3c41f8f1a6828555f2cb01473743bc2f5d345e18
Robin <robin.correa@bizmates.ph>
2019-10-06 20:46:13 +0800

    Create todo feature

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/register', 'Api\AuthController@register');
 Route::post('/login', 'Api\AuthController@login');
+
+Route::post('/todos', 'Api\TodoController@index');
