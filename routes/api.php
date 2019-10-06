c69bbfa811aad1c288bac97071ac46096463030f
Robin <robin.correa@bizmates.ph>
2019-10-06 22:14:56 +0800

    Fix API routes

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
     Route::get('/todos', 'Api\TodoController@index');
     Route::get('/todos/{id}', 'Api\TodoController@show');
